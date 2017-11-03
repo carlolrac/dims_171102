7e3f797b3bf9aa1edff10133fc1d20ebf638caa7
th63vn <carlo13@gmx.com>
2017-11-03 09:42:53 +0000

    so, bin is the way

--- bin/BCRS11/keywinit.pl
+++ bin/BCRS11/keywinit.pl
@@ -49,7 +49,7 @@ HTML
 
 my @keylett = ('A'..'Z','Main');
 foreach (@keylett) {
-	$_ ne "Main" and print "<a href=\"../../cgi-bin/BCRS11/keysearch.pl?keylett=$_\" target=\"main\">$_</a> &nbsp;&nbsp;&nbsp;";
+	$_ ne "Main" and print "<a href=\"../../bin/BCRS11/keysearch.pl?keylett=$_\" target=\"main\">$_</a> &nbsp;&nbsp;&nbsp;";
 	$_ eq "Main" and print "&nbsp;&nbsp;&nbsp;&nbsp;<a href=\"../../index.html\" target=\"_parent\">$_</a>";
 	}
 
